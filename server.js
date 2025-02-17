80807c9bea6375bb20c8134b05076d7a96e9fae9
ShailySarker <shailysarker99@gmail.com>
2025-02-17 11:06:31 +0600

    initial setup completed

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.listen(1800, () => console.log("Server running on port 1800"));
